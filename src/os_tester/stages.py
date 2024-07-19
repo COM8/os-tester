76913b242700c751a8a8cf914da971610cccaa2e
JanSchröter <JanSchrter@JS-AP-2024>
2024-07-19 13:37:50 +0200

    copied stages

--- src/os_tester/stages.py
+++ src/os_tester/stages.py
@@ -1,29 +1,47 @@
 import sys
+
 from os import path
 from typing import Any, Dict, List
-
 import yaml  # type: ignore
 
+class subpath:
+    """
+    A single path with reference image, thresholds and actions to perform once the threshold is reached.
+    """
+    
+    checkFile: str
+    
+    checkMseLeq: float
+    checkSsimGeq: float
+    nextStep: str
+    actions: List[Dict[str, Any]]
+    
+    def __init__(self, pathDict: Dict[str, Any], basePath: str):
+        self.checkFile = path.join(basePath, pathDict["check"]["file"])
+        self.checkMseLeq = pathDict["check"]["mse_leq"]
+        self.checkSsimGeq = pathDict["check"]["ssim_geq"]
+        self.actions = pathDict["actions"] if "actions" in pathDict else list()
+        self.nextStep = pathDict["nextStage"]
 
 class stage:
     """
-    A single stage with reference image, thresholds and actions to perform once the threshold is reached.
+    A single stage with the timeout
     """
-
+    
     name: str
     timeoutS: float
-    checkFile: str
-    checkMseLeq: float
-    checkSsimGeq: float
-    actions: List[Dict[str, Any]]
+    pathsList: List[subpath]
 
     def __init__(self, stageDict: Dict[str, Any], basePath: str):
         self.name = stageDict["stage"]
         self.timeoutS = stageDict["timeout_s"]
-        self.checkFile = path.join(basePath, stageDict["check"]["file"])
-        self.checkMseLeq = stageDict["check"]["mse_leq"]
-        self.checkSsimGeq = stageDict["check"]["ssim_geq"]
-        self.actions = stageDict["actions"] if "actions" in stageDict else list()
+        
+        self.pathsList = list()
+        pathDict: Dict[str, Any]
+        for pathDict in stageDict["paths"]:
+            self.pathsList.append(subpath(pathDict["path"], basePath))
+            
+        
 
 
 class stages:
@@ -34,23 +52,23 @@ class stages:
     basePath: str
     stagesList: List[stage]
 
-    def __load_stages(self) -> None:
+    def __load_stages(self,yamlFileName: str) -> None:
         """
         Loads the stage definition from 'self.basePath' and stores the result inside 'self.stagesList'.
         """
-        ymlFile: str = path.join(self.basePath, "stages.yml")
-        print(f"Loading stages from: {ymlFile}")
+        ymlFilePath: str = path.join(self.basePath,yamlFileName+".yml")
+        print(f"Loading stages from: {ymlFilePath}")
 
-        if not path.exists(ymlFile):
-            print(f"Stage config at '{ymlFile}' not found!")
+        if not path.exists(ymlFilePath):
+            print(f"Stage config at '{ymlFilePath}' not found!")
             sys.exit(2)
 
-        if not path.isfile(ymlFile):
-            print(f"Stage config at '{ymlFile}' is no file!")
+        if not path.isfile(ymlFilePath):
+            print(f"Stage config at '{ymlFilePath}' is no file!")
             sys.exit(3)
 
         stagesDict: Dict[str, Any]
-        with open(ymlFile, "r", encoding="utf-8") as file:
+        with open(ymlFilePath, "r", encoding="utf-8") as file:
             stagesDict = yaml.safe_load(file)
 
         self.stagesList = list()
@@ -58,6 +76,6 @@ class stages:
         for stageDict in stagesDict["stages"]:
             self.stagesList.append(stage(stageDict, self.basePath))
 
-    def __init__(self, basePath: str):
+    def __init__(self, basePath: str,yamlFileName: str):
         self.basePath = basePath
-        self.__load_stages()
+        self.__load_stages(yamlFileName)
